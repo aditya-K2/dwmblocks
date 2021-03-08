3352ae0227f84aef98ae1846a1179ddb64381ea5
aditya-K2 <adityakurdunkar2@gmail.com>
2021-03-08 17:39:15 +0530

    minor changes

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "/home/aditya/suckless/scripts/battery.sh", 1,		0},
+	{"  ", "/home/aditya/suckless/scripts/battery.sh", 1,		0},
 
 	{"", "/home/aditya/suckless/scripts/backlight.sh", 0,		20},
 
 	{"", "/home/aditya/suckless/scripts/pulseaudio.sh", 0,		10},
+	
+	{" ", "/home/aditya/suckless/scripts/bitcoin", 60,		0},
 
-	{" ", "date '+%b %d (%a) %I:%M%p'",					60,		0},
+	{" ", "date '+%b %d (%a) %I:%M%p' ",					60,		0},
 
 };
 
